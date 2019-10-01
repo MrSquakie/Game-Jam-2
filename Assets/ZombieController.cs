a7c769d315e5d684a51203a1d4eac59ec524b36b
MrSquakie <45904658+MrSquakie@users.noreply.github.com>
2019-10-01 14:58:37 -0700

    Fixed Animation

---
+++ Assets/ZombieController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    private Animator anim => GetComponent<Animator>();
+
+    private Rigidbody rb => GetComponentInParent<Rigidbody>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (rb.velocity.magnitude > 0.2f)
+        //{
+            anim.SetFloat("VelX", rb.velocity.x * 5f);
+            anim.SetFloat("VelY", rb.velocity.y * 5f);
+       // }
+    }
+}
