dc09d5213a84c6ac761c88ca2069883916ce5009
Allen <49079375+ajfoust@users.noreply.github.com>
2019-10-05 12:18:23 -0700

    Updated Assets

---
+++ Assets/Plugins/GameCreator/Characters/Actions/ActionCharacterTeleport.cs
@@ -0,0 +1,40 @@
+﻿namespace GameCreator.Characters
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.Events;
+    using GameCreator.Core;
+
+	[AddComponentMenu("")]
+	public class ActionCharacterTeleport : IAction
+	{
+        public TargetCharacter character = new TargetCharacter(TargetCharacter.Target.Player);
+
+        [Space]
+        public TargetPosition position = new TargetPosition(TargetPosition.Target.Position);
+
+        public override bool InstantExecute(GameObject target, IAction[] actions, int index)
+        {
+            Character targetCharacter = this.character.GetCharacter(target);
+            if (targetCharacter == null) return true;
+
+            Vector3 targetPosition = this.position.GetPosition(target);
+            targetCharacter.characterLocomotion.Teleport(targetPosition);
+
+            return true;
+        }
+
+		#if UNITY_EDITOR
+
+        public static new string NAME = "Character/Teleport";
+        private const string NODE_TITLE = "Teleport {0} to {1}";
+
+        public override string GetNodeTitle()
+        {
+            return string.Format(NODE_TITLE, this.character, this.position);
+        }
+
+        #endif
+    }
+}
---
+++ Assets/Plugins/GameCreator/Variables/Mono/Actions/ActionListVariableIterator.cs
@@ -0,0 +1,44 @@
+﻿namespace GameCreator.Variables
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.Events;
+    using GameCreator.Core;
+    using GameCreator.Core.Hooks;
+
+    [AddComponentMenu("")]
+	public class ActionListVariableIterator : IAction
+	{
+        public HelperListVariable listVariables = new HelperListVariable();
+        [Space]
+        public NumberProperty pointer = new NumberProperty(0);
+
+        // EXECUTE METHOD: ------------------------------------------------------------------------
+
+        public override bool InstantExecute(GameObject target, IAction[] actions, int index)
+        {
+            ListVariables list = this.listVariables.GetListVariables(target);
+            if (list == null || list.variables.Count == 0) return true;
+
+            int value = this.pointer.GetInt(target);
+            list.SetInterator(value);
+            return true;
+        }
+
+        #if UNITY_EDITOR
+
+        private const string NODE_TITLE = "Set List Variables {0} iterator";
+        public static new string NAME = "Variables/List Variables Iterator";
+
+        public override string GetNodeTitle()
+        {
+            return string.Format(
+                NODE_TITLE,
+                this.listVariables
+            );
+        }
+
+        #endif
+    }
+}
