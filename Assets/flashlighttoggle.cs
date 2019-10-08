db61841b0b7358989e26dce82dbfea9d6fad3caa
MrSquakie <45904658+MrSquakie@users.noreply.github.com>
2019-10-07 21:10:30 -0700

    Update

---
+++ Assets/flashlighttoggle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashlighttoggle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject light;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            light.active = !light.active;
+        }
+    }
+}
